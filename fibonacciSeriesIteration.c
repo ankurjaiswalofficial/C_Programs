16b9981a46d01ddaa9e51ec3ab79bd10effba4c5
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2023-05-28 09:05:35 +0530

    Fibonacci Series using Iteration

---
+++ fibonacciSeriesIteration.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Fibonacci series using Iteration
+
+int main()
+{
+    int times, num1 = 0, num2 = 1, temp;
+    printf("Enter the number of Fibonacci Numbers Required: ");
+    scanf("%d", &times);
+    printf("Required Fibonacci Series:\n%d %d ", num1, num2);
+    for (int i = 0; i < (times - 2); i++)
+    {
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp;
+        printf("%d ", temp);
+    }
+    return 0;
+}
